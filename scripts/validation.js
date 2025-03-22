1ab4e99fd05ae5d46974fa05667062571baf3084
Rolan Bykov <Rbykov30@gmail.com>
2025-03-21 23:52:39 -0400

    added disabled button

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,13 +1,13 @@
 const showInputError = (formEl, inputEl, errorMsg) => {
     const errorMsgEl = formEl.querySelector(`#${inputEl.id}-error`);
     errorMsgEl.textContent = errorMsg;
-    inputEl.classList.add(".Modal__input_type_error");
+    inputEl.classList.add(".modal__input_type_error");
 };
 
 const hideInputError = (formEl, inputEl,) => {
     const errorMsgEl = formEl.querySelector(`#${inputEl.id}-error`);
     errorMsgEl.textContent = "";
-    inputEl.classList.remove(".Modal__input_type_error");
+    inputEl.classList.remove(".modal__input_type_error");
 };
 
 const checkInputValidity = (formEl, inputEl) => {
@@ -18,17 +18,32 @@ const checkInputValidity = (formEl, inputEl) => {
     }
 }
 
+const hasInvalidInput = (inputList) => {
+    return inputList.some((input) => {
+        return !input.validity.valid;
+    })
+}
+
+const toggleButtonState = (inputList, buttonEl) => {
+    if (hasInvalidInput(inputList)) {
+        buttonEl.disabled = true;
+        buttonEl.classList.add('modal__button_disabled');
+    } else {
+        buttonEl.disabled = false;
+        buttonEl.classList.remove('modal__button_disabled');
+    }
+};
+
 const setEventListeners = (formEl) => {
     const inputList = Array.from(formEl.querySelectorAll(".modal__input"));
-    const buttonElement = formEl.querySelector(".modal__button");
+    const buttonElement = formEl.querySelector(".modal__submit-btn");
     
-    // TODO - handle initial states
-    // toggleButtonState(inputList, buttonElement);
+        toggleButtonState(inputList, buttonElement);
     
     inputList.forEach((inputElement) => {
         inputElement.addEventListener("input", function () {
             checkInputValidity(formEl, inputElement);
-            // toggleButtonState(inputList, buttonElement);
+             toggleButtonState(inputList, buttonElement);
         });
     });
 };
